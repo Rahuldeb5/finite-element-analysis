91be2524fd5adc7aa7d2a27393dbddc82d6bdc41
Rahuldeb5 <rahuldeb.usa@gmail.com>
2026-06-01 19:41:09 -0400

    started working on the tip deflection splitting into multiple parts and solving the euler bernoulli equation

--- main.py
+++ main.py
@@ -34,12 +34,27 @@ label(pos=vec(-5, -3, 1),  text="Z", color=color.blue, box=False)
 label(pos=vec(-3.25, -1.4, 0), text="Fixed end", color=color.black, box=False)
 label(pos=vec(3,-1.4, 0), text="Free end",  color=color.black, box=False)
  
-scene.append_to_caption("\nClick the beam to place a 50 kN downward force arrow.\n")
- 
+# scene.append_to_caption("\nClick the beam to place a 50 kN downward force arrow.\n")
+
+NUM_SEG = 12
+SEG_LEN = 6.0 / NUM_SEG
+beam_segs = []
+for i in range(NUM_SEG):
+    x_cen = -3 + (i + 0.5) * SEG_LEN
+    s = box(pos=vec(x_cen, 0, 0), length=SEG_LEN * 0.97,
+            height=0.5, width=0.5, color=vec(0.5, 0.6, 0.8))
+    beam_segs.append(s)
+
 placed_arrows = []
 placed_labels = []
 net_force_y = 0
 num_forces = 0
+
+is_deflected = False
+ 
+E = 200e9
+I = 0.5 * 0.5**3 / 12.0
+L = 6.0
  
 def get_direction():
     d = dir_menu.selected
@@ -66,9 +81,15 @@ def update_readout(s):
  
 def on_dir(m):
     pass
+
+def reset_beam():
+    for i in range(NUM_SEG):
+        x_cen = -3 + (i + 0.5) * SEG_LEN
+        beam_segs[i].pos = vec(x_cen, 0, 0)
+        beam_segs[i].color = vec(0.5, 0.6, 0.8)
  
 def clear_forces(b):
-    global net_force_y, num_forces
+    global net_force_y, num_forces, is_deflected
     for a in placed_arrows:
         a.visible = False
     for lbl in placed_labels:
@@ -77,8 +98,27 @@ def clear_forces(b):
     placed_labels.clear()
     net_force_y = 0
     num_forces = 0
+    is_deflected = False
     count_text.text = "0"
     net_text.text = "0 kN"
+    deflect_text.text = "0.00 mm"
+    reset_beam()
+
+def show_deflection(b):
+    global is_deflected
+    F = net_force_y * 1000
+    SCALE = 300
+    max_deflect = F * L**3 / (3 * E * I)
+    for i in range(NUM_SEG):
+        x_from_wall = (i + 0.5) * SEG_LEN
+        deflection = F * (3 * L * x_from_wall**2 - x_from_wall**3) / (6 * E * I)
+        x_cen = -3 + (i + 0.5) * SEG_LEN
+        t = 1.0 - (i / NUM_SEG)
+        seg_color = vec(t, 1 - t * 0.6, 0.2)
+        beam_segs[i].pos = vec(x_cen, deflection * SCALE, 0)
+        beam_segs[i].color = seg_color
+    deflect_text.text = str(round(max_deflect * 1000, 3)) + " mm"
+    is_deflected = True
  
 def place_force(evt):
     global net_force_y, num_forces
@@ -123,10 +163,14 @@ scene.append_to_caption("\n\n  Direction:  ")
 dir_menu = menu(choices=["Down", "Up", "Left", "Right"], bind=on_dir)
 scene.append_to_caption("\n\n  ")
 clear_btn = button(text="Clear all forces", bind=clear_forces)
+scene.append_to_caption("    ")
+deflect_btn = button(text="Show deflection", bind=show_deflection)
 scene.append_to_caption("\n\n  Forces placed: ")
 count_text = wtext(text="0")
 scene.append_to_caption("\n  Net Y force: ")
 net_text = wtext(text="0 kN")
+scene.append_to_caption("\n  Tip deflection: ")
+deflect_text = wtext(text="0.00 mm")
 scene.append_to_caption("\n\n  Click the beam to place an arrow.\n")
  
 scene.bind("click", place_force)
